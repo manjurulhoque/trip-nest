72fcbdaf85b7f9c9e33b98e82542cb147ab0bcdf
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2026-03-15 21:42:43 +0600

    ref: add django_extensions to installed apps for enhanced development tools
    - Included the "django_extensions" app in the settings to provide additional management commands and utilities for development.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
     "drf_spectacular",
+    "django_extensions",
     # Custom apps
     "core",
     "hotels",
